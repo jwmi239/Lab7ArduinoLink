6b3cd41c4b26d8f52ec22bc9c4eee9c5f977ccd4
jwmi239 <jake.miller@uky.edu>
2025-03-10 15:43:35 -0400

    Addition
    Added 3 test lines

--- Lab7/Lab7.ino
+++ Lab7/Lab7.ino
@@ -3,7 +3,9 @@ void setup() {
 
 }
 
-
+const int PWMB = 10; //speed control pin on the motor driver for the left motor
+const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
+const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
 
 void loop() {
   // put your main code here, to run repeatedly:
